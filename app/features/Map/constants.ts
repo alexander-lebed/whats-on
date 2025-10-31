5f59e6436997f06f7bfea58a6beefd1da559a7c5
Alexander Lebed <lebed.alexander90@gmail.com>
2025-10-31 18:31:10 +0100

    Update the coordinates of the Province of Castellón

--- app/features/Map/constants.ts
+++ app/features/Map/constants.ts
@@ -5,8 +5,8 @@ export const CASTELLON_CENTER: google.maps.LatLngLiteral = { lat: 39.985, lng: -
 
 // Approximate bounding box for Castellón de la Plana (Benicàssim, Castellon, Villarreal)
 export const CASTELLON_BOUNDS: google.maps.LatLngBoundsLiteral = {
-  south: 39.91,
-  west: -0.14,
-  north: 40.06,
-  east: 0.09,
+  south: 39.75, // near Nules / Almenara
+  west: -0.45, // near Villafranca del Cid / Ares del Maestrat
+  north: 40.65, // near Morella / Forcall
+  east: 0.35, // Mediterranean coast near Vinaròs
 };
